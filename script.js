3c2b0f4dbc32bab40e17e901db36e6d79ba5bb71
zeenat <zeenu.23@gmail.com>
2024-03-12 11:45:08 +0530

    initialization

---
+++ script.js
@@ -0,0 +1,4 @@
+//1. Use a free API from the internet and feed your app with live data.
+//2. Create a note saving app which stores your note to localstorage.
+//3. Fetch the note which was saved.
+//4. Delete the note
