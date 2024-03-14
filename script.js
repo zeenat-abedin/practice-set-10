6ed7d45303311ba6d4293ff95a44d2c48f31daf7
zeenat <zeenu.23@gmail.com>
2024-03-14 15:58:19 +0530

    Add Bootstrap to index.html and add a navbar.

--- script.js
+++ script.js
@@ -2,3 +2,29 @@
 //2. Create a note saving app which stores your note to localstorage.
 //3. Fetch the note which was saved.
 //4. Delete the note
+
+let url = "https://contests.net/api/v1/all"; 
+    
+// let method = "GET";
+// fetch(url, {method: method})
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+    
+// let saveNote = (title, body)=>{
+//     let note ={
+//         title : title ,
+//         body: body
+//         }
+//     let notes = JSON.parse(localStorage.getItem("notes")) || [];
+//     notes.push(note);
+//     localStorage.setItem("notes",JSON.stringify(notes));
+// }
+
+// let getSavedNotes=()=>{
+//     let notes = JSON.parse(localStorage.getItem("notes"));  
+//     if(!notes){
+//         return [];
+//     } else {
+//         return notes;
+//         }
+//     }
